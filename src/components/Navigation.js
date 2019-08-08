c4e77805d93f1ccca2cc7aa15a971672e136c833
brit-starks <brittneytechiethomas@gmail.com>
2019-08-07 20:26:18 -0700

    React Router Basic Nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
@@ -6,12 +7,15 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
+          <Link to='/'>Home</Link>
           <a href="">Home</a>
         </div>
         <div>
+          <Link to ='/about'>About</Link>
           <a href="">About</a>
         </div>
         <div>
+          <Link to='/contacts'>Contacts</Link>
           <a href="">Contact</a>
         </div>
       </div>
